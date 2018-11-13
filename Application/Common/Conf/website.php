f264ea3b7c8061d6f5e6a7b333ba567aaca52d23
guopong <chianbine@163.com>
2018-11-13 15:46:56 +0800

    注释掉website.php 中的DOMAIN配制

--- Application/Common/Conf/website.php
+++ Application/Common/Conf/website.php
@@ -1,7 +1,7 @@
 <?php 
 		return array(
 			'WEB_TITLE' => 'API支付系统',
-			'DOMAIN' => 'www.ourspay.com.cn',
+			//'DOMAIN' => 'www.ourspay.com.cn',
 			'MODULE_ALLOW_LIST'   => array('Home','User','Manage','Install', 'Weixin','Pay','Cashier','Agent','Payment'),
 			'URL_MODULE_MAP'  => array('manage'=>'admin', 'agent'=>'user', 'user'=>'user'),
 			'LOGINNAME' => 'pay91888',
